bc1a51f0bc78bd42013bc847d48645720365cc94
Amy Townsend <amygtownsend@gmail.com>
2018-12-05 16:10:40 -0500

    add screen height hero image, new css font, cuisine info window data, cuisine category filter, story content

--- js/scripts.js
+++ js/scripts.js
@@ -18,12 +18,7 @@ $(function(){
       console.log(restaurants);
       restaurants.forEach(function(restaurant){
         if (restaurant.restaurant.year_founded !== "closed") {
-          locations[i] = [restaurant.restaurant.name, parseFloat(restaurant.restaurant.location.latitude, 10), parseFloat(restaurant.restaurant.location.longitude, 10), restaurant.restaurant.year_founded];
-          var cuisines = restaurant.restaurant.cuisines;
-          var h;
-          for (h = 0; h < cuisines.length; h++) {
-            locations[i][h + 4] = cuisines[h];
-          }
+          locations[i] = [restaurant.restaurant.name, parseFloat(restaurant.restaurant.location.latitude, 10), parseFloat(restaurant.restaurant.location.longitude, 10), restaurant.restaurant.year_founded, restaurant.restaurant.cuisines]
           i++
         }
       });
@@ -47,18 +42,42 @@ function initMap() {
   var marker, j;
 
   setTimeout(function(){
+    var markers2 = [];
     for (j = 0; j < locations.length; j++) {
+      var cuisines = locations[j][4];
       marker = new google.maps.Marker({
         position: new google.maps.LatLng(locations[j][1], locations[j][2]),
-        map: map
+        map: map,
+        category: cuisines
       });
+
+      markers2.push(marker);
 
       google.maps.event.addListener(marker, 'click', (function(marker, j) {
         return function() {
-          infowindow.setContent('<h2>' + locations[j][0] + '</h2><p>Year added: ' + locations[j][3] + '</p>');
+          var cuisinesAsString = locations[j][4].join(', ');
+          infowindow.setContent('<h2>' + locations[j][0] + '</h2><p>Year opened: ' + locations[j][3] + '</p>' + '<p>Cuisine(s): ' + cuisinesAsString);
           infowindow.open(map, marker);
         }
       })(marker, j));
     }
+
+    filterMarkers = function(category) {
+      for (i = 0; i < markers2.length; i++) {
+        marker = markers2[i];
+
+        // If is same category or category not picked
+        if((typeof marker.category == 'object' && marker.category.indexOf(category) >= 0) || category.length == 0){
+          marker.setVisible(true);
+        }
+        // Categories don't match
+        else
+        {
+          marker.setVisible(false);
+        }
+      }
+    }
+
+    initialize();
   }, 1000)
 }
