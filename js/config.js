9dd7c54c2d1ac8f370040839d2fdf52fd6772ade
Amy Townsend <amygtownsend@gmail.com>
2018-12-05 11:55:17 -0500

    reformatted data, added year founded, added js to use Zomato API with Google Maps API, added story content and css

--- js/config.js
+++
@@ -1,3 +0,0 @@
-var config = {
-  MY_KEY : 'c9d3c18a863392c0164fb4a3e8beec89'
-};
--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,64 @@
+var locations = [];
+
 $(function(){
   console.log('scripts loaded');
 
-  var myKey = config.MY_KEY;
-  var url = 'https://developers.zomato.com/api/v2.1/search?entity_id=119518&entity_type=subzone&q=Franklin%20St&sort=rating&apikey=' + myKey;
-  var url2 = 'https://developers.zomato.com/api/v2.1/search?entity_id=119518&entity_type=subzone&q=Franklin%20St&start=20&sort=rating&apikey='+ myKey;
-  var url3 = 'https://developers.zomato.com/api/v2.1/search?entity_id=119518&entity_type=subzone&q=Franklin%20St&start=40&sort=rating&apikey='+ myKey;
-  var url4 = 'https://developers.zomato.com/api/v2.1/search?entity_id=119518&entity_type=subzone&q=Franklin%20St&start=60&sort=rating&apikey='+ myKey;
-  var url5 = 'https://developers.zomato.com/api/v2.1/search?entity_id=119518&entity_type=subzone&q=Franklin%20St&start=80&sort=rating&apikey='+ myKey;
-  var urlArray = [url, url2, url3, url4, url5];
-  var data = [];
-  var html = '';
-  var i = '';
+  var url = './js/restaurants.json'
+  var restaurants = [];
+  var i = 0;
 
-  for (i = 0; i < urlArray.length; i++) {
-    $.ajax({
-      type: 'GET',
-      url: urlArray[i],
-      dataType: 'json',
-      async: true,
-      data: data,
-      success: function(data){
-        console.log(data);
-      }
-    });
-  }
+
+  $.ajax({
+    type: 'GET',
+    url: url,
+    dataType: 'json',
+    async: true,
+    data: restaurants,
+    success: function(restaurants){
+      console.log(restaurants);
+      restaurants.forEach(function(restaurant){
+        if (restaurant.restaurant.year_founded !== "closed") {
+          locations[i] = [restaurant.restaurant.name, parseFloat(restaurant.restaurant.location.latitude, 10), parseFloat(restaurant.restaurant.location.longitude, 10), restaurant.restaurant.year_founded];
+          var cuisines = restaurant.restaurant.cuisines;
+          var h;
+          for (h = 0; h < cuisines.length; h++) {
+            locations[i][h + 4] = cuisines[h];
+          }
+          i++
+        }
+      });
+    }
+  });
 });
+
+console.log(locations);
+
+function initMap() {
+  var franklinSt = {lat: 35.912749, lng: -79.0617988};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: franklinSt,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  var infowindow = new google.maps.InfoWindow();
+
+  var marker, j;
+
+  setTimeout(function(){
+    for (j = 0; j < locations.length; j++) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[j][1], locations[j][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, j) {
+        return function() {
+          infowindow.setContent('<h2>' + locations[j][0] + '</h2><p>Year added: ' + locations[j][3] + '</p>');
+          infowindow.open(map, marker);
+        }
+      })(marker, j));
+    }
+  }, 1000)
+}
