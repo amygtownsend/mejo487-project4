95e8d823cd2db4c79b1cfd00a4c89ad4c165de15
Amy Townsend <amygtownsend@gmail.com>
2018-12-10 20:57:06 -0500

    add comments

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ $(function(){
   var restaurants = [];
   var i = 0;
 
-
+// AJAX call to get data from json file
   $.ajax({
     type: 'GET',
     url: url,
@@ -15,9 +15,13 @@ $(function(){
     async: true,
     data: restaurants,
     success: function(restaurants){
+      // Console log array of restaurant data
       console.log(restaurants);
+      // Loop through each restaurant
       restaurants.forEach(function(restaurant){
+        // Skip closed restaurants
         if (restaurant.restaurant.year_founded !== "closed") {
+          // Create locations array of restaurant name, latitude, longitude, year opened and array of cuisines
           locations[i] = [restaurant.restaurant.name, parseFloat(restaurant.restaurant.location.latitude, 10), parseFloat(restaurant.restaurant.location.longitude, 10), restaurant.restaurant.year_founded, restaurant.restaurant.cuisines]
           i++
         }
@@ -26,9 +30,12 @@ $(function(){
   });
 });
 
+// Console log locations array
 console.log(locations);
 
+// Google maps function
 function initMap() {
+  // Location for the center of the interactive map
   var franklinSt = {lat: 35.912749, lng: -79.0617988};
 
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -41,49 +48,67 @@ function initMap() {
 
   var marker, j;
 
+  // Time out function for locations array to be built before creating markers
   setTimeout(function(){
+    // Initialize markers array
     var markers2 = [];
+    // Loop through locations array
     for (j = 0; j < locations.length; j++) {
+      // RGB values for each marker determined by the year opened
       var r = 25 + ((locations[j][3] - 1922) / 96) * (209);
       var g = 0 + ((locations[j][3] - 1922) / 96) * (229);
       var b = 127 + ((locations[j][3] - 1922) / 96) * (128);
       var icon = {
+        // Custom SVG path for google marker
         path: 'M 34.07 1.75 C 24.44 -2.04 13.51 0.46 6.68 7.99 c -7.11 7.85 -8.79 17.98 -3.86 27.24 c 3.5 6.59 7.79 12.75 11.62 19.17 c 4.44 7.45 8.47 15.08 8.75 24.07 c 0.04 1.29 0.08 3.04 1.84 2.95 c 1.69 -0.09 1.64 -1.84 1.67 -3.13 c 0.2 -7.38 3.03 -13.93 6.62 -20.15 c 4.07 -7.05 8.48 -13.89 12.72 -20.84 c 2.38 -3.9 3.72 -8.14 3.83 -11.64 C 49.86 14.8 43.34 5.39 34.07 1.75 Z M 24.87 33.81 c -4.96 -0.04 -9.17 -4.49 -8.91 -9.43 c 0.24 -4.75 4.56 -8.72 9.31 -8.54 c 4.76 0.18 8.78 4.43 8.67 9.18 C 33.82 29.77 29.63 33.85 24.87 33.81 Z',
+        // Fill color of each marker's custom RGB value
         fillColor: 'rgb(' + r + ', ' + g + ', ' + b + ')',
         fillOpacity: 1,
+        // Anchors the marker at the bottom center, where the "point" is
         anchor: new google.maps.Point(27,86),
         scale: .5,
         strokeColor: 'rgb(25, 0, 127)',
         strokeWeight: 1
       };
+      // Initialize cuisines variable to cuisines array within locations array
       var cuisines = locations[j][4];
+      // Create marker for each location
       marker = new google.maps.Marker({
+        // Set position to latitude and longitude of each location
         position: new google.maps.LatLng(locations[j][1], locations[j][2]),
         map: map,
+        // Set category to cuisines array
         category: cuisines,
+        // Set icon to marker SVG path with custom fill color
         icon: icon
       });
 
+      // Add each marker to markers array
       markers2.push(marker);
 
+      // Click event on each marker opens info window
       google.maps.event.addListener(marker, 'click', (function(marker, j) {
         return function() {
+          // Change cuisines array into string joined by commas
           var cuisinesAsString = locations[j][4].join(', ');
+          // Info window shows location name, year opened and cuisines on click
           infowindow.setContent('<h2>' + locations[j][0] + '</h2><p>Year opened: ' + locations[j][3] + '</p>' + '<p>Cuisine(s): ' + cuisinesAsString);
           infowindow.open(map, marker);
         }
       })(marker, j));
     }
 
+    // Create filter by cuisine category
     filterMarkers = function(category) {
+      // Loop through markers array
       for (i = 0; i < markers2.length; i++) {
         marker = markers2[i];
 
-        // If is same category or category not picked
+        // Show marker if one of its cuisines matches the cuisine selected or if no cuisine has been selected
         if((typeof marker.category == 'object' && marker.category.indexOf(category) >= 0) || category.length == 0){
           marker.setVisible(true);
         }
-        // Categories don't match
+        // Hide marker if its cuisines don't match the cuisine selected
         else
         {
           marker.setVisible(false);
