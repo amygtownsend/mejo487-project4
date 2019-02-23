462a0c35aefb3cf1bf9be4523961f973a4d4c33b
Amy Townsend <amygtownsend@gmail.com>
2019-02-23 15:42:22 -0500

    Round RGB values for markers

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
-var locations = [];
-
 document.addEventListener("DOMContentLoaded", function () {
   console.log('scripts loaded');
 
   var url = './js/restaurants.json'
+  var locations = [];
   var i = 0;
 
   // Use Fetch API to get data
@@ -102,6 +101,10 @@ document.addEventListener("DOMContentLoaded", function () {
     chart.renderTo('#chart');
     return locations;
   }).then((locations) => {
+
+    // Console log locations array
+    console.log(locations);
+
     // Google maps function
     function initMap() {
       // Location for the center of the interactive map
@@ -122,9 +125,9 @@ document.addEventListener("DOMContentLoaded", function () {
       // Loop through locations array
       for (j = 0; j < locations.length; j++) {
         // RGB values for each marker determined by the year opened
-        var r = 25 + ((locations[j][3] - 1922) / 96) * (209);
-        var g = 0 + ((locations[j][3] - 1922) / 96) * (229);
-        var b = 127 + ((locations[j][3] - 1922) / 96) * (128);
+        var r = Math.round(25 + ((locations[j][3] - 1922) / 96) * (209));
+        var g = Math.round(0 + ((locations[j][3] - 1922) / 96) * (229));
+        var b = Math.round(127 + ((locations[j][3] - 1922) / 96) * (128));
         var icon = {
           // Custom SVG path for google marker
           path: 'M 34.07 1.75 C 24.44 -2.04 13.51 0.46 6.68 7.99 c -7.11 7.85 -8.79 17.98 -3.86 27.24 c 3.5 6.59 7.79 12.75 11.62 19.17 c 4.44 7.45 8.47 15.08 8.75 24.07 c 0.04 1.29 0.08 3.04 1.84 2.95 c 1.69 -0.09 1.64 -1.84 1.67 -3.13 c 0.2 -7.38 3.03 -13.93 6.62 -20.15 c 4.07 -7.05 8.48 -13.89 12.72 -20.84 c 2.38 -3.9 3.72 -8.14 3.83 -11.64 C 49.86 14.8 43.34 5.39 34.07 1.75 Z M 24.87 33.81 c -4.96 -0.04 -9.17 -4.49 -8.91 -9.43 c 0.24 -4.75 4.56 -8.72 9.31 -8.54 c 4.76 0.18 8.78 4.43 8.67 9.18 C 33.82 29.77 29.63 33.85 24.87 33.81 Z',
@@ -186,6 +189,3 @@ document.addEventListener("DOMContentLoaded", function () {
     initMap();
   });
 });
-
-// Console log locations array
-console.log(locations);
